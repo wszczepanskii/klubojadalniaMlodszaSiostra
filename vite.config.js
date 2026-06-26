b512350718236a4826e50466483dba61913deb78
wiktor <wiktor.szczepanski2001@gmail.com>
2026-06-26 17:12:49 +0200

    name fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.GITHUB_PAGES === 'true'
-    ? '/klubokawiarniaMlodszaSiostra/'
+    ? '/klubojadalniaMlodszaSiostra/'
     : '/',
 })
